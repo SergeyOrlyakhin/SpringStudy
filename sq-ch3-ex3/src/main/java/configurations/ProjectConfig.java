06683a4a974d1971cfb2b0ec79094474f5f52815
Сергей Орляхин <Orlyakhin@lanit.ru>
2023-09-18 17:58:01 +0400

    [+] sq-ch3-ex3

---
+++ sq-ch3-ex3/src/main/java/configurations/ProjectConfig.java
@@ -0,0 +1,26 @@
+package configurations;
+
+import beans.Parrot;
+import beans.Person;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ProjectConfig {
+
+    @Bean
+    Person person(Parrot parrot){
+        Person p = new Person();
+        p.setName("Lola");
+        p.setParrot(parrot); // Здесь не создается новый объект, а берется из контекста уже существующий
+        //т.к. спринг отслеживает вызовы методов и при необходимости применяет определенную логику
+        return p;
+    }
+
+    @Bean
+    Parrot parrot(){
+        Parrot p = new Parrot();
+        p.setName("Kiki");
+        return p;
+    }
+}
